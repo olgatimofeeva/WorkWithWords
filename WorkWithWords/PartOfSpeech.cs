e4018d6afa65201f33a74707f63e29bbe90fa4c8
Olga Timofeeva <38052124+olgatimofeeva@users.noreply.github.com>
2018-10-23 23:32:02 +0300

    Add classes
    Добавила два класса: части речи, спецификация.

---
+++ WorkWithWords/PartOfSpeech.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkWithWords
+{
+    class PartOfSpeech
+    {
+
+        public PartOfSpeech(int ID, string partOfspeech)
+        {
+            this.ID = ID;
+            this.partOfspeech = partOfspeech;
+
+        }
+
+        public int ID { get; set; }
+        public string partOfspeech { get; set; }
+    }
+}
---
+++ WorkWithWords/Specification.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkWithWords
+{
+    class Specification
+    {
+
+        public Specification(int ID, string specification)
+        {
+            this.ID = ID;
+            this.specification = specification;
+
+        }
+
+        public int ID { get; set; }
+        public string specification { get; set; }
+
+
+    }
+}
